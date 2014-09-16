767a9d86699bcd0e701723ad12fdd7ad51a9bc54
liaoxuezhi <liaoxuezhi@baidu.com>
2014-09-16 15:11:59 +0800

    update

--- fis-conf.js
+++ fis-conf.js
@@ -3,3 +3,11 @@ fis.config.set('namespace', 'lsdemo');
 fis.config.set('pack', {
     'all.js': 'widget/**/*.js'
 });
+
+fis.config.get('roadmap.path').unshift({
+    reg : /^\/ls\-diff\-plugin\/(plugin\/.*\.php)$/i,
+    release: '/$1'
+});
+
+// npm install -g fis-postpackager-lsdiff-map
+fis.config.set('module.postpackager', 'lsdiff-map');
